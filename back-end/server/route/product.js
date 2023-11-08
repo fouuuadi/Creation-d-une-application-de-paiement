223c7c15e1b44e1206d8fba9842eb19f323be541
fouuuadi <123892942+fouuuadi@users.noreply.github.com>
2023-11-08 20:41:08 +0100

    La route pour les récupérer les produits fonctionne

--- back-end/server/route/product.js
+++ back-end/server/route/product.js
@@ -1,13 +1,22 @@
 const express = require("express");
 const router = new express.Router();
+const connection = require("../db")
 
-router.get("/", async(req, res) => {
+router.post("/allProducts", async(req, res) => {
     try {
-        console.log(req.body)
-        res.send(200, { content: "ok" })
+        const queryAllProducts = "SELECT * FROM products";
+        connection.query(queryAllProducts, (error, results) => {
+            if(error) {
+                console.error(error);
+                res.status(500).send("Erreur lors de l'exécution de la requête SQL.");
+            } else {
+                res.status(200).json(results);
+            }
+        });
     } catch (e) {
         console.error(e.message)
+        res.status(500).send("Erreur lors de la gestion de la requête.");
     }
-})
+});
 
 module.exports = router;
