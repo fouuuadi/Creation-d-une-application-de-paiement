ffb2228a1510f71a5c503c25c8804c23912edcfd
fouuuadi <123892942+fouuuadi@users.noreply.github.com>
2023-11-12 01:49:42 +0100

    fix css card payment

--- front-end/src/components/pages/data-client/payment.jsx
+++ front-end/src/components/pages/data-client/payment.jsx
@@ -23,55 +23,56 @@ const Payment = () => {
     };
 
     return (
-
         <div>
-            <div>
-                <HeaderPayment/>
+            <HeaderPayment />
+            <div className="credit-card-form">
+                <h2>Formulaire de paiement par carte de crédit</h2>
+                <form onSubmit={handleSubmit}>
+                    <div className="form-group">
+                        <label>Numéro de carte de crédit</label>
+                        <input
+                            type="text"
+                            name="cardNumber"
+                            value={formData.cardNumber}
+                            onChange={handleInputChange}
+                            placeholder="1234 5678 9012 3456"
+                        />
+                    </div>
+                    <div className="form-group">
+                        <label>Titulaire de la carte</label>
+                        <input
+                            type="text"
+                            name="cardHolder"
+                            value={formData.cardHolder}
+                            onChange={handleInputChange}
+                            placeholder="John Doe"
+                        />
+                    </div>
+                    <div className="form-group">
+                        <label>Date d'expiration</label>
+                        <input
+                            type="text"
+                            name="expirationDate"
+                            value={formData.expirationDate}
+                            onChange={handleInputChange}
+                            placeholder="MM/YY"
+                        />
+                    </div>
+                    <div className="form-group">
+                        <label>CVV</label>
+                        <input
+                            type="text"
+                            name="cvv"
+                            value={formData.cvv}
+                            onChange={handleInputChange}
+                            placeholder="123"
+                        />
+                    </div>
+                    <button type="submit" className="pay-button">
+                        Payer
+                    </button>
+                </form>
             </div>
-            <h2>Formulaire de paiement par carte de crédit</h2>
-            <form onSubmit={handleSubmit}>
-                <div>
-                    <label>Numéro de carte de crédit</label>
-                    <input
-                        type="text"
-                        name="cardNumber"
-                        value={formData.cardNumber}
-                        onChange={handleInputChange}
-                        placeholder="1234 5678 9012 3456"
-                    />
-                </div>
-                <div>
-                    <label>Titulaire de la carte</label>
-                    <input
-                        type="text"
-                        name="cardHolder"
-                        value={formData.cardHolder}
-                        onChange={handleInputChange}
-                        placeholder="John Doe"
-                    />
-                </div>
-                <div>
-                    <label>Date d'expiration</label>
-                    <input
-                        type="text"
-                        name="expirationDate"
-                        value={formData.expirationDate}
-                        onChange={handleInputChange}
-                        placeholder="MM/YY"
-                    />
-                </div>
-                <div>
-                    <label>CVV</label>
-                    <input
-                        type="text"
-                        name="cvv"
-                        value={formData.cvv}
-                        onChange={handleInputChange}
-                        placeholder="123"
-                    />
-                </div>
-                <button type="submit">Payer</button>
-            </form>
         </div>
     );
 };
